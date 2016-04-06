7d623610877839e137c8a7e6455893fc0f700960
Pedro Cesar <pedrocesar.ti@gmail.com>
2016-04-06 00:42:37 -0300

    Removed timestamp problem.

--- scripts/init_test_connection.sh
+++ scripts/init_test_connection.sh
@@ -10,8 +10,8 @@ do
 	DOWNLOAD=$(cat $FILE | grep "Download:" | awk -F " " '{print $2}')
 	UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
 	echo "Download: $DOWNLOAD Upload: $UPLOAD    $TIMESTAMP"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD $TIMESTAMP"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD $TIMESTAMP"
+	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
+	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
 	sleep $TIME_SLEEP
 
 done
