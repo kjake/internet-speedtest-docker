4ac1d9317b115d34b6f07575c02c028f0ca9abe8
Pedro Cesar <pedrocesar.ti@gmail.com>
2016-04-09 08:11:56 -0300

    Change variable time name.

--- scripts/init_test_connection.sh
+++ scripts/init_test_connection.sh
@@ -12,6 +12,6 @@ do
 	echo "Download: $DOWNLOAD Upload: $UPLOAD    $TIMESTAMP"
 	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
 	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
-	sleep $TIME_SLEEP
+	sleep $TEST_INTERVAL
 
 done
